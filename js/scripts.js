65501ba1cf746c4f634af6f02a356be248bf91ae
Padma Patil <padma@email.com>
2018-11-02 09:01:42 -0700

    make notes on format and approach. Begin business logic in js.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//Business Logic
+
+fuction Pizza (){
+  this.size = ["small", "medium", "large"];
+  this.toppings = ["pepperoni", "sausage", "cheese", "jalapenio", "chicken", "tomato"];
+}
+
+Pizza.prototype.Price = function(){
+  var sizePrice = 12;
+  var sizes = ["small", "medium", "large"]
+
+}
