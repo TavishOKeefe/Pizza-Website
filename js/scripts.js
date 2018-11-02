a48c654eb10bd1e91ba6f7186639f17c5c0ca9b9
Padma Patil <padma@email.com>
2018-11-02 10:17:49 -0700

    add js business logic to possible completion.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 //Business Logic
 
+var yourPizza = sizePrice + costOfTopping;
+
 function Pizza (){
   this.size = ["small", "medium", "large"];
-  this.toppings = ["pepperoni", "sausage", "cheese", "jalapenio", "chicken", "tomato"];
+  this.topping = ["pepperoni", "sausage", "cheese", "jalapenio", "chicken", "tomato"];
+  this.finalPrice = yourPizza;
 }
 
 Pizza.prototype.Price = function(){
@@ -15,10 +18,31 @@ Pizza.prototype.Price = function(){
     sizePrice + 10;
   }
   return sizePrice;
+  console.log(sizePrice);
 }
+
 
 Pizza.prototype.toppingPrice = function(){
   var toppingSelection = ["pepperoni", "sausage", "cheese", "jalapenio", "chicken", "tomato"];
-  var addedCostOfToppings = 1.5;
+  var costOfTopping = 1.5;
 
+  for (var i = 0 ; i < toppingSelection.length; i ++) {
+
+    if (toppingSelection[i] === 0) {
+      costOfTopping + 0;
+    } else if (toppingSelection[i] === 1) {
+      costOfTopping + 3;
+    } else if (toppingSelection[i] === 2) {
+      costOfTopping + 5;
+    } else if (toppingSelection[i] === 3) {
+      costOfTopping + 6;
+    } else if (toppingSelection[i] === 4) {
+      costOfTopping + 7;
+    } else if (toppingSelection[i] === 5) {
+      costOfTopping + 8;
+    }
+
+  }
+  return costOfTopping;
+  console.log(costOfTopping);
 }
