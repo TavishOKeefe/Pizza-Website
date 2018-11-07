502f77fe88fdbc4b3d550c8a4c3fea03e0fe05ce
Padma Patil <padma@email.com>
2018-11-06 19:03:08 -0800

    cleaned code further.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,12 @@
 //Business Logic:
 
-
 function Pizza (size, topping, price) {
   this.size = size,
   this.topping = topping,
   this.priceSize = 0
-  // this.priceTopping = 0,
-  // this.sizeList = ["small", "medium", "large"];
-  // this.toppingList = ["pepperoni", "sausage", "cheese", "jalapenio", "chicken", "tomato"];
 }
 
 Pizza.prototype.price = function(){
-  // var sizeSelection = this.size
-  // var typeSelection = this.topping
   var sizePrice = this.priceSize;
 
     if (this.size === "Small") {
@@ -37,30 +31,6 @@ Pizza.prototype.price = function(){
     return sizePrice;
 }
 
-
-// Pizza.prototype.toppingPrice = function(){
-//   var toppingSelection = this.topping;
-//   var costOfTopping = this.priceTopping;
-//
-//   for (var i = 0 ; i < toppingSelection.length; i ++) {
-//     if (toppingSelection[i] === "pepperoni") {
-//       costOfTopping += 1.5;
-//     } else if (toppingSelection[i] === "sausage") {
-//       costOfTopping += 4.5;
-//     } else if (toppingSelection[i] === "cheese") {
-//       costOfTopping += 6.5;
-//     } else if (toppingSelection[i] === "jalapenio") {
-//       costOfTopping += 7.5;
-//     } else if (toppingSelection[i] === "chicken") {
-//       costOfTopping += 8.5;
-//     } else if (toppingSelection[i] === "tomato") {
-//       costOfTopping += 9.5;
-//     }
-//
-//   }
-//     return costOfTopping;
-// }
-
 //User interface logic:
 
 $(document).ready(function() {
@@ -70,24 +40,6 @@ $(document).ready(function() {
 
     var inputSize = $("#pizzaSize").val();
     var inputToppings = $("#pizzaType").val();
-
-  //   $("input:checkbox[name=pizza-topping]:checked").each(function() {
-  //     var toppingCheck = $(this).val();
-  //     inputToppings.push(toppingCheck);
-  //     console.log(toppingCheck);
-  // })
-      // var toppings = document.getElementById("#toppings");
-      // var tops = toppings.getElementsByTagName("INPUT");
-      // for (var i=0; i < tops.length; i ++){
-      //   if (tops[i].type === 'checkbox') {
-      //     tops[i].onclick = function(){
-      //       inputToppings.push(checkedValue);
-      //     }
-      //   }
-      // }
-
-    // var inputToppings = [];
-    // console.log(inputToppings);
     var userPizza = new Pizza(inputSize, inputToppings);
 
     displayResults(userPizza);
@@ -96,11 +48,8 @@ $(document).ready(function() {
 
   function displayResults(pizzaResults){
     var pizzaSizePriceResults = pizzaResults.price();
-    // var pizzaToppingPriceResults = pizzaResults.toppingPrice();
 
     $("#result").text(pizzaSizePriceResults);
-    // $("#result2").text(pizzaToppingPriceResults);
-
   }
 
 });
